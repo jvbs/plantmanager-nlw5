b98e93549d1f5cc7f5a00ab389f99894d6470d81
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2021-04-19 16:04:09 -0300

    welcome screen created with custom ts types and state/props from react

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { Welcome } from "./src/pages/Welcome";
 
 const App = () => {
   return (
     <>
-      <View style={style.container}>
-        <Text>NLW #5</Text>
-      </View>
+      <Welcome/>
     </>
   );
 };
