09acf8fe9d2b56c5fa41f7416896bc26df0b09b7
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2021-04-20 13:32:25 -0300

    codigo refatorado

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react'
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native'
 import colors from '../../styles/colors'
 
 import wateringImg from '../assets/watering.png'
-import Button from '../components/Button'
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false)
-
-  function handleVisibility(){
-    setVisible(true)
-  }
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
         Gerencie {'\n'} suas plantas {'\n'} de forma fácil
       </Text>
-      { visible && <Image source={wateringImg} style={styles.image} /> }
+
+      <Image source={wateringImg} style={styles.image} resizeMode="contain"/>
+
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. 
         Nós cuidamos de lembrar você sempre que precisar.
       </Text>
 
-      <Button title={"Avançar"} onPress={handleVisibility}/>
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>
+          >
+        </Text>
+      </TouchableOpacity>
 
     </SafeAreaView>
   )
@@ -33,7 +32,7 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   title: {
     fontSize: 32,
@@ -55,15 +54,14 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    // width: 56
-    paddingHorizontal: 10
-  },
-  image: {
-    width: 292,
-    height: 284
+    width: 56
   },
   buttonText: {
     color: colors.white,
     fontSize: 24
-  }
+  },
+  image: {
+    width: 292,
+    height: Dimensions.get('window').width*0.7
+  },
 })
