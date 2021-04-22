e986504c3ba7e6095de907de82f5e9abb15a3d65
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2021-04-22 15:55:17 -0300

    investigando erro cardprimary

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+import { SvgFromUri } from 'react-native-svg'
+
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
@@ -14,6 +16,7 @@ interface PlantProps extends RectButtonProps {
 export function PlantCardPrimary({ data, ...rest }: PlantProps) {
   return (
     <RectButton style={styles.container} {...rest}>
+      <SvgFromUri uri={data.photo} width={70} height={70}/>
       <Text style={styles.text}>{ data.name }</Text>
     </RectButton>
   )
